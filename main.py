437ee6c35c4aee8354a80f6e518bdf0afe954ec6
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-01-12 00:15:46 +0100

    Testing footer.html and header.html.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/header")
+def header():
+    return render_template("header.html")
+
+
+@app.route("/footer")
+def footer():
+    return render_template("footer.html")
 
 
 if __name__ == "__main__":
