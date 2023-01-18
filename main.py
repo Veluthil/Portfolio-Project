612e6d2f1d17d3845905f75f08d23abd1c8d6e8b
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-01-19 00:49:04 +0100

    Changes in custom-styles.css, footer.html, index.html. Add content in contact.html, about.html and main.py.

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from flask_bootstrap import Bootstrap5
+from flask import Flask
+import smtplib
+import os
+from dotenv import load_dotenv
+import bleach
 
+
+#DOTENV
+load_dotenv("D:/Programming/PythonEnV/.env.txt")
+MY_EMAIL = os.getenv("MY_EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+bootstrap = Bootstrap5(app)
+
+
+##SANITIZING HTML
+def strip_invalid_html(content):
+    allowed_tags = ['a', 'abbr', 'acronym', 'address', 'b', 'br', 'div', 'dl', 'dt',
+                    'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
+                    'li', 'ol', 'p', 'pre', 'q', 's', 'small', 'strike',
+                    'span', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
+                    'thead', 'tr', 'tt', 'u', 'ul']
+    allowed_attrs = {
+        'a': ['href', 'target', 'title'],
+        'img': ['src', 'alt', 'width', 'height'],
+    }
+    cleaned = bleach.clean(content,
+                           tags=allowed_tags,
+                           attributes=allowed_attrs,
+                           strip=True)
+    return cleaned
 
 
 @app.route("/")
@@ -19,9 +50,32 @@ def projects():
     return render_template("projects.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        if data["name"] and data["email"] and data["message"] != "":
+            send_mail(strip_invalid_html(data["name"]),
+                      strip_invalid_html(data["email"]),
+                      strip_invalid_html(data["phone"]),
+                      strip_invalid_html(data["message"]))
+        else:
+            flash("You can't send a message without filling name, email and message fields.")
+            return redirect(url_for("contact", msg_sent=False))
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_mail(name, email, phone, message):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=email_message
+        )
 
 
 if __name__ == "__main__":
