b924e7b36e6a8e27622e72ee07c5bc5a9c2158c2
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-01-20 19:29:32 +0100

    Changes and new content in .html, .css and .js.

---
+++ static/js/custom-js.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  var banner_height = $("#navscroll").height();
+  var lastScrollTop = 0;
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var currScrollTop = $(this).scrollTop();
+    if (scroll >= banner_height && currScrollTop > lastScrollTop) {
+      $("#banner").hide();
+    } else {
+      $("#banner").show();
+    }
+    lastScrollTop = currScrollTop;
+
+  });
+
+});
